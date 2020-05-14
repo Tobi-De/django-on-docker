42d22e215c36728cdcb02e74ff385867844131c2
Tobi-De <tobidegnon@protonmail.com>
2020-05-14 22:07:13 +0100

    add uplaod app and nginx config

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'upload'
 ]
 
 MIDDLEWARE = [
@@ -121,4 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
